40c2c0e7c8cb7fbc42cbd19969bc0b283ddc2123
Promise Ghosh Chowdhury <promise.official000@gmail.com>
2025-02-02 23:53:08 +0600

    dashboard added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navigation } from "@/components/Navigation";
-// import { SessionProvider } from "next-auth/react";
-// import { auth } from "@/auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,26 +23,14 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const session = await auth();
-  // if (session?.user) {
-  //   // TODO: Look into https://react.dev/reference/react/experimental_taintObjectReference
-  //   // filter out sensitive data before passing to client.
-  //   session.user = {
-  //     name: session.user?.name,
-  //     email: session.user.email,
-  //     image: session.user.image,
-  //   };
-  // }
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
         suppressHydrationWarning
       >
-        {/* <SessionProvider session={session}> */}
         <Navigation />
         <main>{children}</main>
-        {/* </SessionProvider> */}
       </body>
     </html>
   );
